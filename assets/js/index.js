f883fdc4f13160a19c0232d9751a7a659b59db0c
agasanchez <ana.ga.sanchez@gmail.com>
2023-11-16 14:33:41 -0600

    Initial Code

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch("https://2prtlwlksrciwllkr5xj7nd2pm0hzpns.lambda-url.us-east-2.on.aws/");
+    if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+    let data = await response.json();
+
+
+    counter.innerHTML = `This page has ${data} Views!`;
+}
+
+updateCounter();
