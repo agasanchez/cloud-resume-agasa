cb971784d9a28a890689f6490db2f7471aa1b660
agasanchez <ana.ga.sanchez@gmail.com>
2024-01-04 14:42:57 -0600

    testing new lambda config

--- website/js/index.js
+++ website/js/index.js
@@ -17,36 +17,36 @@ var express = requiere('express')
     (() => {
         const form = document.querySelector('form');
         const submitResponse = document.querySelector('#email_response');
-        const formURL = 'https://bu7n9doxa9.execute-api.us-east-1.amazonaws.com/default/sendemail';
+        const formURL = 'https://s6drfz3t3dzf6cnu6gc3hxwrzq0scxhi.lambda-url.us-east-1.on.aws/';
 
         form.onsubmit = e => {
-        e.preventDefault();
+            e.preventDefault();
 
-        // Capture the form data
-        let data = {};
-        Array.from(form).map(input => (data[input.id] = input.value));
-        console.log('Sending: ', JSON.stringify(data));
-        submitResponse.innerHTML = 'Sending...'
+            // Capture the form data
+            let data = {};
+            Array.from(form).map(input => (data[input.id] = input.value));
+            console.log('Sending: ', JSON.stringify(data));
+            submitResponse.innerHTML = 'Sending...'
 
-        // Create the AJAX request
-        var xhr = new XMLHttpRequest();
-        xhr.open(form.method, formURL, true);
-        //xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
-        xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        //xhr.setRequestHeader('Access-Control-Allow-Origin:', 'http://anasanchez.net');
+            // Create the AJAX request
+            var xhr = new XMLHttpRequest();
+            xhr.open(form.method, formURL, true);
+            //xhr.setRequestHeader('Accept', 'application/json; charset=utf-8');
+            xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+            //xhr.setRequestHeader('Access-Control-Allow-Origin:', 'http://anasanchez.net');
 
-        // Send the collected data as JSON
-        xhr.send(JSON.stringify(data));
+            // Send the collected data as JSON
+            xhr.send(JSON.stringify(data));
 
-        xhr.onloadend = response => {
-            console.log('Status: ', response.target.status);
-            if (response.target.status === 200) {
-            form.reset();
-            submitResponse.innerHTML = 'Form submitted. Success!';
-            } else {
-            submitResponse.innerHTML = 'Error! Please try again.';
-            console.error(JSON.parse(response));
-            }
-        };    
+            xhr.onloadend = response => {
+                console.log('Status: ', response.target.status);
+                if (response.target.status === 200) {
+                form.reset();
+                submitResponse.innerHTML = 'Form submitted. Success!';
+                } else {
+                submitResponse.innerHTML = 'Error! Please try again.';
+                console.error(JSON.parse(response));
+                }
+            };    
         };
     });
